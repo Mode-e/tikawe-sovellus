335a339947bc9ed1d435a2fbd76448c398543b99
Mode-e <teemu.mode@yahoo.fi>
2026-09-04 15:52:10 +0300

    Updated login

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
+from werkzeug.security import generate_password_hash
+import sqlite3
+import db
 
 app = Flask(__name__)
 
@@ -22,6 +25,33 @@ def my_shifts():
 def register():
     return render_template("register.html")
 
-@app.route("/login")
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"
+
+@app.route("/login", methods=["POST"])
 def login():
-    return render_template("login.html")
+    username = request.form["username"]
+    password = request.form["password"]
+    
+    sql = "SELECT password_hash FROM users WHERE username = ?"
+    password_hash = db.query(sql, [username])[0][0]
+
+    if check_password_hash(password_hash, password):
+        session["username"] = username
+        return redirect("/")
+    else:
+        return "VIRHE: väärä tunnus tai salasana"
